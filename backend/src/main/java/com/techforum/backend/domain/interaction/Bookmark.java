f2912bd017dbc7dde0fbd38ec57d6d29c5f25872
Ahmed Ashraf <ahmmdashraf1@gmail.com>
2026-05-01 00:03:30 +0300

    feat: Initialized SQL code & hibernate ORM

--- backend/src/main/java/com/techforum/backend/domain/interaction/Bookmark.java
+++ backend/src/main/java/com/techforum/backend/domain/interaction/Bookmark.java
@@ -1,4 +1,41 @@
 package com.techforum.backend.domain.interaction;
 
-public class Bookmark {
+import com.techforum.backend.common.persistence.BaseEntity;
+import com.techforum.backend.domain.thread.Thread;
+import com.techforum.backend.domain.user.User;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.Instant;
+
+@Entity
+@Table(
+        name = "Bookmarks",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"user_id", "thread_id"})
+        }
+)
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Bookmark extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "thread_id", nullable = false,  updatable = false)
+    @NotNull
+    private Thread thread;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false,  updatable = false)
+    @NotNull
+    private User user;
+
+    @Column(name = "created_at", updatable = false, nullable = false)
+    @CreationTimestamp
+    private Instant createdAt;
+
 }
--- backend/src/main/java/com/techforum/backend/domain/thread/Thread.java
+++ backend/src/main/java/com/techforum/backend/domain/thread/Thread.java
@@ -1,4 +1,60 @@
 package com.techforum.backend.domain.thread;
 
-public class Thread {
+import com.techforum.backend.common.persistence.BaseEntity;
+import com.techforum.backend.domain.tag.Tag;
+import com.techforum.backend.domain.thread.enums.ThreadStatus;
+import com.techforum.backend.domain.user.User;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity(name = "Thread")
+@Table(name = "Threads")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Thread extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    @NotNull
+    private User author;
+
+    @Column(length = 255, nullable = false, updatable = true)
+    @Size(max = 255)
+    @NotBlank
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false, updatable = true)
+    @NotBlank
+    private String body;
+
+    @Column(nullable = false, updatable = true)
+    @NotNull
+    @JdbcTypeCode(SqlTypes.NAMED_ENUM)
+    private ThreadStatus status;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreationTimestamp
+    private Instant createdAt;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "thread_tags",
+            joinColumns = @JoinColumn(name = "thread_id"),
+            inverseJoinColumns = @JoinColumn(name = "tag_id")
+    )
+    @Builder.Default
+    private Set<Tag> tags = new HashSet<>();
 }
