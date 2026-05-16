5f95c87d6f59720b7671f7921b2140d45fd9d550
saifullah <siafmosad@gmail.com>
2026-05-16 11:25:29 +0300

    feat: add users list

--- backend/src/main/java/com/techforum/backend/domain/user/UserController.java
+++ backend/src/main/java/com/techforum/backend/domain/user/UserController.java
@@ -1,3 +1,24 @@
 package com.techforum.backend.domain.user;
 
-public class UserController {}
+import com.techforum.backend.domain.user.dtos.UserDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+  private final UserService userService;
+
+  @GetMapping
+  public ResponseEntity<Page<UserDTO>> listUsers(@RequestParam(defaultValue = "0") int page,
+                                                 @RequestParam(defaultValue = "10") int size) {
+    Page<UserDTO> userPage = userService.listUsers(page, size);
+    return ResponseEntity.ok(userPage);
+  }
+}
--- backend/src/main/java/com/techforum/backend/domain/user/UserService.java
+++ backend/src/main/java/com/techforum/backend/domain/user/UserService.java
@@ -1,3 +1,22 @@
 package com.techforum.backend.domain.user;
 
-public class UserService {}
+import com.techforum.backend.domain.user.dtos.UserDTO;
+import com.techforum.backend.domain.user.mappers.UserMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+  private final UserRepository userRepository;
+
+  public Page<UserDTO> listUsers(int page, int size) {
+    Pageable pageable = PageRequest.of(page, size, Sort.by("username").ascending());
+    Page<User> userPage = userRepository.findAll(pageable);
+    return userPage.map(UserMapper::toDTO);
+  }
+}
