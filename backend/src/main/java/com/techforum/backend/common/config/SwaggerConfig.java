33ee55d9328a0fb1571083002ab37804f706a04e
saifullah <siafmosad@gmail.com>
2026-06-27 05:27:15 +0300

    chore(docs): add SwaggerConfig for API documentation

---
+++ backend/src/main/java/com/techforum/backend/common/config/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package com.techforum.backend.common.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+  @Bean
+  public OpenAPI openAPI() {
+    return new OpenAPI()
+        .info(
+            new Info()
+                .title("TechForum API")
+                .version("1.0.0")
+                .description("REST API for the TechForum platform"))
+        .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+        .components(
+            new Components()
+                .addSecuritySchemes(
+                    "Bearer Authentication",
+                    new SecurityScheme()
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")));
+  }
+}
